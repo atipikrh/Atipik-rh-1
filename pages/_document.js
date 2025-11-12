74dedcb72e633f177de68ee7d08de03dbcc82d7d
atipikrh <contact@atipikrh.com>
2025-11-12 16:09:12 +0100

    feat: ajoute google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,11 @@ export default function Document() {
         {/* Preconnect pour optimiser le chargement des fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T45Z2XRQ');`
+          }}
+        />
         
         {/* Préchargement de la police Brittany */}
         <link rel="preload" href="/Fonts/BrittanySignature.ttf" as="font" type="font/ttf" crossOrigin="" />
@@ -55,6 +60,14 @@ export default function Document() {
         />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T45Z2XRQ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
