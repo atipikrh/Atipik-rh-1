c7dac2e0941e9d029c256ec50579e291cb5aa11c
atipikrh <contact@atipikrh.com>
2025-11-21 10:02:37 +0100

    Mise à jour du libellé 'Les expertes' en 'Les spécialistes' dans le menu et la page Notre équipe

--- components/Header.js
+++ components/Header.js
@@ -252,7 +252,7 @@ export default function Header({ isFixed = false, isHomePage = false }) {
                     className="block px-4 py-3 text-sm text-[#013F63] hover:bg-blue-50 hover:text-[#012a4a] transition-colors border-t border-gray-100"
                   >
                     <div className="font-medium">Notre équipe</div>
-                    <div className="text-xs text-blue-400">Les expertes qui vous accompagnent</div>
+                    <div className="text-xs text-blue-400">Les spécialistes qui vous accompagnent</div>
                   </Link>
                   <Link 
                     href="/certification" 
